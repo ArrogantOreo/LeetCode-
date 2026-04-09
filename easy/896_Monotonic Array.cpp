afdaed4b6c44fb63bcc1099790a5b8362e851ac0
ArrogantOreo <tangdong0929@163.com>
2026-04-09 15:56:45 +0800

    单调数列

---
+++ easy/896_Monotonic Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        int flag=1;
+        bool zeng=true,jian=true;
+        int len=nums.size();
+        if(len==1){
+            return true;
+        }
+        for(int i=0;i<len-1;i++){
+            if(nums[i]>nums[i+1]){
+                zeng=false;
+            }
+            if(nums[i]<nums[i+1]){
+                jian=false;
+            }
+        }
+    return zeng || jian;
+    }
+};
